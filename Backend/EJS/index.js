416f5949dc3b94f32b7f931bcd91957ef38205e7
Bhavesh Kerkar <154862336+bhaveshkerkar@users.noreply.github.com>
2025-09-25 02:28:01 +0530

    add all files

--- Backend/EJS/index.js
+++ Backend/EJS/index.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const path = require("path"); // Import the path module
 
 const port = 8080;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // Set the views directory
 
-app.set("views", path.join(__dirname, "views"));
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("about");
 });
 
 app.listen(port, () => {
